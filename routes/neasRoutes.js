c7a405be3b6b0f0750ff9b28d6e57296b6a3186f
pablomoga9 <moyagarcia99@gmail.com>
2022-08-30 11:31:35 +0200

    edit neas routes

--- routes/neasRoutes.js
+++ routes/neasRoutes.js
@@ -3,8 +3,8 @@ const neasControllers = require("../controllers/neasControllers");
 const neasRouter = express.Router();
 
  neasRouter.get('/astronomy/neas', neasControllers.getNeas);
- neasRouter.post('/astronomy/neas', neasControllers.createNea);
- neasRouter.delete('/astronomy/neas', neasControllers.deleteNea);
- neasRouter.put('/astronomy/neas', neasControllers.updateNea);
+ neasRouter.post('/astronomy/neas/create', neasControllers.createNea);
+ neasRouter.delete('/astronomy/neas/delete', neasControllers.deleteNea);
+ neasRouter.put('/astronomy/neas/update', neasControllers.updateNea);
 
  module.exports = neasRouter;
